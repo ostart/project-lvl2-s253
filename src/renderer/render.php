55b3749859a663f7eb9d702fd8fa4efd7c38fd6e
Artem Osta <ostart@mail.ru>
2018-05-11 11:29:29 +0300

    Task 5: Renderer switch to associative array was changed

--- src/renderer/render.php
+++ src/renderer/render.php
@@ -4,12 +4,8 @@ namespace Render;
 
 function rendAst($ast, $format)
 {
-    switch ($format) {
-        case 'pretty':
-            return rendPretty($ast);
-        case 'plain':
-            return rendPlain($ast);
-        case 'json':
-            return rendJson($ast);
-    }
+    $renderList = [ 'pretty' => rendPretty($ast),
+                    'plain'  => rendPlain($ast),
+                    'json'   => rendJson($ast)];
+    return $renderList[$format];
 }
